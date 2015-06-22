ee5ddb12ffd6d0f089fac2e75358f3572ae986a2
Eduardo Acha <eduardoacha@gmail.com>
2015-06-21 22:45:53 -0300

    Added Exception to SSLConnection

---
+++ src/cl/tesis/SSLConnectionException.java
@@ -0,0 +1,11 @@
+package cl.tesis;
+
+public class SSLConnectionException extends Exception {
+
+    public SSLConnectionException() {
+    }
+
+    public SSLConnectionException(String message) {
+        super(message);
+    }
+}
