0b2b1d07ad7af642b3ee1cfa1fcb31357542726d
Eduardo Acha <eduardoacha@gmail.com>
2015-06-22 23:26:40 -0300

    Added finally sentence to getServerCertificate

--- src/cl/tesis/SSLUtil.java
+++ src/cl/tesis/SSLUtil.java
@@ -5,10 +5,22 @@ import java.security.cert.X509Certificate;
 
 public class SSLUtil {
 
-    public static Certificate getServerCertificate(String ip, boolean validate) throws SSLConnectionException, IOException {
-        SSLConnection sslConnection =  new SSLConnection(ip, 443, validate);
-        sslConnection.connect();
-        X509Certificate certificates = (X509Certificate) sslConnection.getServerCertificate();
-        return new Certificate(certificates, ip, validate);
+    public static Certificate getServerCertificate(String ip, boolean validate) throws SSLConnectionException, IOException{
+        SSLConnection sslConnection = null;
+        X509Certificate certificate;
+        try {
+            sslConnection =  new SSLConnection(ip, 443, validate);
+            sslConnection.connect();
+            certificate = (X509Certificate) sslConnection.getServerCertificate();
+        } catch (SSLConnectionException e) {
+            System.out.println("SSLConnectionException");
+            throw e;
+        } finally {
+            if (sslConnection != null) {
+                sslConnection.close();
+            }
+        }
+
+        return new Certificate(certificate, ip, validate);
     }
 }
