cef408bc836f839156213a1f6ab742a1ef8733b8
Eduardo Acha <eduardoacha@gmail.com>
2015-06-30 12:43:21 -0300

    Added logging to CSVFileReader

--- src/cl/tesis/input/CSVFileReader.java
+++ src/cl/tesis/input/CSVFileReader.java
@@ -3,22 +3,25 @@ package cl.tesis.input;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class CSVFileReader implements  FileReader{
+    private static final int TICKS = 1;
     private static final String CSV_SEPARATOR = ",";
-
     private static final Logger logger = Logger.getLogger(CSVFileReader.class.getName());
 
+    private int readLines;
     private String fileName;
     private BufferedReader reader;
 
     public CSVFileReader(String fileName) {
+        this.readLines = 0;
         this.fileName = fileName;
         try {
             this.reader = new BufferedReader(new java.io.FileReader(this.fileName));
         } catch (FileNotFoundException e) {
-            logger.info("File not found");
+            logger.log(Level.INFO, "File not found {0}", this.fileName);
             System.exit(0);
         }
 
@@ -30,6 +33,12 @@ public class CSVFileReader implements  FileReader{
 
         try{
             line = this.reader.readLine();
+            this.readLines++;
+
+            if (this.readLines % TICKS == 0 && line!= null) {
+                logger.log(Level.INFO, "Lines processed {0}", this.readLines);
+            }
+
         } catch (IOException error) {
             return null;
         }
