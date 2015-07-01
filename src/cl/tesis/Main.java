943086a8f0623a2e80b642b3ed468f4ef5b46896
Eduardo Acha <eduardoacha@gmail.com>
2015-07-01 14:28:59 -0300

    Deleted unused import

--- src/cl/tesis/Main.java
+++ src/cl/tesis/Main.java
@@ -5,7 +5,6 @@ import cl.tesis.ssl.ConnectionThreads;
 import cl.tesis.input.CSVFileReader;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
