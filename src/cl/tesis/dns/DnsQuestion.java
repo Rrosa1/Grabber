73b70073b3d09a9298fc78d42d64d84485f6111c
Eduardo Acha <eduardoacha@gmail.com>
2015-08-19 13:04:15 -0300

    Added parameter to the end of question block

--- src/cl/tesis/dns/DnsQuestion.java
+++ src/cl/tesis/dns/DnsQuestion.java
@@ -5,19 +5,23 @@ public class DnsQuestion {
     private String qname;
     private int qtype;
     private int qclass;
+    private int finishQuestion;
 
     public DnsQuestion(byte[] data) {
         this.qname = "";
         int finishUrl = this.parseUrl(data);
+
         this.qtype = DnsUtil.byteArraytoInt(data, finishUrl + 1, finishUrl + 2);
         this.qclass = DnsUtil.byteArraytoInt(data, finishUrl + 3, finishUrl + 4);
+
+        this.finishQuestion = finishUrl + 5;
     }
 
     private int parseUrl(byte[] data){
         int i;
         boolean first = true;
 
-        for (i = 0; i < data.length; i++) {
+        for (i = 12; i < data.length; i++) {
             if (data[i] == 0)
                 break;
             else if (first && data[i] <= 31)
@@ -30,6 +34,10 @@ public class DnsQuestion {
         return i;
     }
 
+    public int getFinishQuestion() {
+        return finishQuestion;
+    }
+
     @Override
     public String toString() {
         return "DnsQuestion{" +
@@ -39,10 +47,10 @@ public class DnsQuestion {
                 '}';
     }
 
-    public static void main(String[] args) {
-        byte[] STANDARD_QUESTION = DnsUtil.hexStringToByteArray("0377777706756368696c6502636c0000010001");
-        DnsQuestion question =  new DnsQuestion(STANDARD_QUESTION);
-        System.out.println(question.toString());
-    }
+//    public static void main(String[] args) {
+//        byte[] STANDARD_QUESTION = DnsUtil.hexStringToByteArray("0377777706756368696c6502636c0000010001");
+//        DnsQuestion question =  new DnsQuestion(STANDARD_QUESTION);
+//        System.out.println(question.toString());
+//    }
 
 }
