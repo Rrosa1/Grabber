fa2f31c8b27c464718e1cc9cd47c4c45791adf65
Eduardo Acha <eduardoacha@gmail.com>
2015-08-20 15:55:16 -0300

    Created a exception to rais when parse a problematic datagram

---
+++ src/cl/tesis/dns/section/DnsParserException.java
@@ -0,0 +1,12 @@
+package cl.tesis.dns.section;
+
+public class DnsParserException extends Exception{
+
+    public DnsParserException() {
+    }
+
+    public DnsParserException(String message) {
+        super(message);
+    }
+    
+}
