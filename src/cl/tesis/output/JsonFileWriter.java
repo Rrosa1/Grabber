52d0213ae08e254949f11fcc81524443852ad433
Eduardo Acha <eduardoacha@gmail.com>
2015-09-12 19:20:43 -0300

    Fixed some  errors, when print the data in json format

--- src/cl/tesis/output/JsonFileWriter.java
+++ src/cl/tesis/output/JsonFileWriter.java
@@ -7,12 +7,15 @@ import java.util.logging.Logger;
 public class JsonFileWriter implements FileWriter{
 
     private static final Logger logger = Logger.getLogger(JsonFileWriter.class.getName());
+//    private static final String start = "[\n";
 
     private String fileName;
     private BufferedWriter writer;
+    private boolean header;
 
     public JsonFileWriter(String fileName) {
         this.fileName = fileName;
+        this.header = true;
 
         try {
             this.writer =  new BufferedWriter(new OutputStreamWriter(new FileOutputStream(this.fileName), "utf-8"));
@@ -27,11 +30,20 @@ public class JsonFileWriter implements FileWriter{
 
     @Override
     public synchronized void writeLine(Writable writable) throws IOException {
+        if (this.header){
+            this.writer.write("[");
+            this.header = false;
+        } else {
+            this.writer.write(",\n");
+        }
         this.writer.write(((JsonWritable)writable).toJson());
     }
 
     @Override
     public void close() throws IOException {
+        if (!this.header){
+            this.writer.write("]");
+        }
         this.writer.close();
     }
 }
