0a8ab72cb30704a12fad4d0d76e950bebbcd9109
Eduardo Acha <eduardoacha@gmail.com>
2015-09-17 23:28:50 -0300

    Removed unused code

--- src/cl/tesis/http/HttpResponse.java
+++ src/cl/tesis/http/HttpResponse.java
@@ -14,9 +14,7 @@ public class HttpResponse implements CSVWritable, JsonWritable{
     private String ip;
     private String responseCode;
     private String server;
-//    private String contentType;
     private String domain;
-//    private List<String> cookies;
 
     private Map<String, List<String>> header;
     private String index;
@@ -25,10 +23,7 @@ public class HttpResponse implements CSVWritable, JsonWritable{
         this.ip = ip;
         this.responseCode =  getKey(header, null);
         this.server = getKey(header, "Server");
-//        this.contentType =  getKey(header, "Content-Type");
-//        this.cookies = header.get("Set-Cookie");
         this.domain = this.getDomain(header.get("Set-Cookie"));
-        // new elements
         this.header = header;
         this.index = index;
     }
@@ -55,15 +50,6 @@ public class HttpResponse implements CSVWritable, JsonWritable{
         return null;
     }
 
-//    private String getHeader(Map<String, List<String>> header) {
-//        StringBuffer response = new StringBuffer();
-//        for (Map.Entry<String, List<String>> entry : header.entrySet()) {
-//            response.append(entry.getKey());
-//            response.append(entry.getValue());
-//        }
-//
-//    }
-
     @Override
     public String toString() {
         return "HttpResponse{" +
@@ -83,9 +69,7 @@ public class HttpResponse implements CSVWritable, JsonWritable{
         parameters.add("ip");
         parameters.add("response code");
         parameters.add("server");
-//        parameters.add("content type");
         parameters.add("domain");
-//        parameters.add("cookies");
         parameters.add("header");
         parameters.add("index");
 
@@ -99,9 +83,7 @@ public class HttpResponse implements CSVWritable, JsonWritable{
         values.add(this.ip);
         values.add(this.responseCode);
         values.add(this.server);
-//        values.add(this.contentType);
         values.add(this.domain);
-//        values.add(this.cookies + "");
         values.add(new GsonBuilder().disableHtmlEscaping().create().toJson(this.header));
         values.add(this.index);
 
