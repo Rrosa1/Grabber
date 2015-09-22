df8a36f63c82fd18e550955e2b43c1f13c977a86
Eduardo Acha <eduardoacha@gmail.com>
2015-09-22 12:40:43 -0300

    Added new option to print the available modules

--- src/cl/tesis/CommandLine.java
+++ src/cl/tesis/CommandLine.java
@@ -22,23 +22,14 @@ public class CommandLine {
     @Option(name = "-t", aliases = {"--threads"}, metaVar = "Int", usage = "Threads used to send probes", help = true)
     private int threads = 1;
 
-    public String getInput() {
-        return input;
-    }
+    @Option(name = "--list-probe-modules", usage = "Print all probe modules", required = false, help = true)
+    private boolean listProbeModules;
 
-    public String getOutput() {
-        return output;
-    }
+    @Option(name = "--list-output-modules", usage = "Print all output modules", required = false, help = true)
+    private boolean listOutputModules;
 
-    public String getOutputModule() {
-        return outputModule;
-    }
-
-    public int getThreads() { return threads; }
-
-    public String getModule() {
-        return module;
-    }
+    private String[] probeModules = {"SSLCertificate", "HTTPHeader", "SSHVersion"};
+    private String[] outputModules = {"CSV", "JSON"};
 
     public void parse(String[] args) {
         CmdLineParser parser = new CmdLineParser(this);
@@ -52,12 +43,57 @@ public class CommandLine {
             System.exit(0);
         }
 
+        if (listProbeModules) {
+            System.out.println("Probe Modules:");
+
+            for (String m : probeModules)
+                System.out.println(m);
+
+            System.exit(0);
+        }
+        else if (listOutputModules) {
+            System.out.println("Output Modules:");
+
+            for (String m : outputModules)
+                System.out.println(m);
+
+            System.exit(0);
+        }
+
+    }
+
+    private boolean runningArgs() {
+        return getInput() == null || getOutput() == null || getModule() == null;
+    }
+
+    private boolean infoArgs() {
+        return !(listProbeModules || listOutputModules);
     }
 
     public void required(CmdLineParser parser) throws CmdLineException {
-        if (getInput() == null || getOutput() == null || getModule() == null) {
+        if (runningArgs() && infoArgs()) {
             throw new CmdLineException(parser, Messages.FORMAT_ERROR_FOR_MAP, "Require options");
         }
+    }
+
+    public String getInput() {
+        return input;
+    }
+
+    public String getOutput() {
+        return output;
+    }
+
+    public String getOutputModule() {
+        return outputModule;
+    }
+
+    public int getThreads() {
+        return threads;
+    }
+
+    public String getModule() {
+        return module;
     }
 
     public static void main(String[] args) {
