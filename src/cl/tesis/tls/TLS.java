64996aa5836b5e2dde7c4570c7ac6af714dcfd31
Eduardo Acha <eduardoacha@gmail.com>
2015-10-07 18:36:07 -0300

    Validate the certificate chain

--- src/cl/tesis/tls/TLS.java
+++ src/cl/tesis/tls/TLS.java
@@ -1,17 +1,32 @@
 package cl.tesis.tls;
 
+import cl.tesis.mail.SMTP;
+import cl.tesis.mail.SMTPData;
 import cl.tesis.mail.StartTLS;
+import cl.tesis.ssl.HostCertificate;
 import cl.tesis.tls.exception.HandshakeHeaderException;
 import cl.tesis.tls.exception.StartTLSException;
 import cl.tesis.tls.exception.TLSHeaderException;
 
+import java.io.ByteArrayInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.net.SocketTimeoutException;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.cert.CertPathValidatorException;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
 import java.util.Arrays;
+import java.util.List;
+
+import static cl.tesis.tls.CertChainValidator.validateKeyChain;
 
 public class TLS {
 
@@ -31,8 +46,7 @@ public class TLS {
         this.buffer = new byte[BUFFER_SIZE];
     }
 
-
-    public void doHandshake() throws IOException, TLSHeaderException, HandshakeHeaderException {
+    public HostCertificate doHandshake() throws IOException, TLSHeaderException, HandshakeHeaderException, CertificateException, InvalidAlgorithmParameterException, NoSuchAlgorithmException, NoSuchProviderException {
 
         /* client hello */
         this.out.write(new ClientHello(TLSVersion.TLS_11.getStringVersion(), TLSCipherSuites.test).toByte());
@@ -47,15 +61,17 @@ public class TLS {
         /* certificate */
         CertificateMessage certificateMessage = new CertificateMessage(buffer, serverHello.endOfServerHello());
         System.out.println(certificateMessage);
+
+        return this.byteArrayToX509Certificate(certificateMessage.getCertificates());
     }
 
-    public void doMailHandshake(StartTLS start) throws IOException, StartTLSException, TLSHeaderException, HandshakeHeaderException {
+    public HostCertificate doMailHandshake(StartTLS start) throws IOException, StartTLSException, TLSHeaderException, HandshakeHeaderException, CertificateException, InvalidAlgorithmParameterException, NoSuchAlgorithmException, NoSuchProviderException {
         /* Start mail handshake */
         if (!this.startMailHandshake(start)) {
             throw new StartTLSException();
         }
 
-        this.doHandshake();
+        return this.doHandshake();
     }
 
     public ScanTLSVersion checkTLSVersions(StartTLS start) throws IOException, StartTLSException {
@@ -129,9 +145,37 @@ public class TLS {
         return totalRead;
     }
 
-    public static void main(String[] args) throws IOException, StartTLSException {
-        Socket s =  new Socket("64.64.18.121", 110);
-        TLS tls = new TLS(s);
-        System.out.println(tls.checkTLSVersions(StartTLS.POP3).toJson());
+    public HostCertificate byteArrayToX509Certificate(List<byte[]> list) throws CertificateException, InvalidAlgorithmParameterException, NoSuchAlgorithmException, NoSuchProviderException {
+        X509Certificate[] certs = new X509Certificate[list.size()];
+        CertificateFactory certificateFactory = CertificateFactory.getInstance("X.509");
+        int i = 0;
+
+        for (byte[] array : list) {
+            InputStream in = new ByteArrayInputStream(array);
+            certs[i] = (X509Certificate)certificateFactory.generateCertificate(in);
+            ++i;
+        }
+
+        X509Certificate[] chain;
+        if (i == 1) {
+            chain = null;
+        } else {
+            chain = Arrays.copyOfRange(certs, 1, certs.length);
+        }
+
+        return new HostCertificate(certs[0], this.socket.getInetAddress().toString(), validateKeyChain(certs[0], chain), chain);
+    }
+
+    public static void main(String[] args) throws IOException, StartTLSException, TLSHeaderException, HandshakeHeaderException, CertificateException, KeyStoreException, NoSuchAlgorithmException, InvalidAlgorithmParameterException, CertPathValidatorException, NoSuchProviderException {
+
+//        SMTP smtp =  new SMTP("64.64.18.121");
+//        SMTPData data = new SMTPData("64.64.18.121", smtp.startSMTP(), smtp.sendHELP(), smtp.sendEHLO());
+//
+//        TLS tls = new TLS(smtp.getSocket());
+//
+//        X509Certificate[] x509Certificates = tls.doMailHandshake(StartTLS.SMTP);
+//
+//        System.out.println(validateKeyChain(x509Certificates[0], Arrays.copyOfRange(x509Certificates, 1, x509Certificates.length)));
+
     }
 }
