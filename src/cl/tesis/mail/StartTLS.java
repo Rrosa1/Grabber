80db7686f1ab8b4d12b616697d896121af1595b9
Eduardo Acha <eduardoacha@gmail.com>
2015-10-26 20:49:00 -0300

    Added protocol name to START TLS

--- src/cl/tesis/mail/StartTLS.java
+++ src/cl/tesis/mail/StartTLS.java
@@ -1,16 +1,22 @@
 package cl.tesis.mail;
 
 public enum StartTLS {
-    SMTP("STARTTLS\r\n", "220"),
-    POP3("STLS\r\n", "+OK"),
-    IMAP("a001 STARTTLS\r\n", "OK");
+    SMTP("SMTP", "STARTTLS\r\n", "220"),
+    POP3("POP3", "STLS\r\n", "+OK"),
+    IMAP("IMAP", "a001 STARTTLS\r\n", "OK");
 
+    private final String protocol;
     private final String message;
     private final String responce;
 
-    StartTLS(String msg, String resp) {
-       this.message = msg;
-       this.responce = resp;
+    StartTLS(String protocol, String message, String responce) {
+        this.protocol = protocol;
+        this.message = message;
+        this.responce = responce;
+    }
+
+    public String getProtocol() {
+        return protocol;
     }
 
     public String getMessage() {
