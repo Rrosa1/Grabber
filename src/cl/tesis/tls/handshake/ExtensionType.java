2ff052b5c176eabdf8015bacf1271dd1bceaca2d
Eduardo Acha <eduardoacha@gmail.com>
2015-10-26 21:33:38 -0300

    Change format of Extension type

--- src/cl/tesis/tls/handshake/ExtensionType.java
+++ src/cl/tesis/tls/handshake/ExtensionType.java
@@ -1,30 +1,59 @@
 package cl.tesis.tls.handshake;
 
-/**
- * Created by eduardo on 18-10-15.
- */
+
+import java.util.Arrays;
+
 public enum ExtensionType {
-    SERVER_NAME((byte)0x00),
-    MAX_FRAGMENT_LENGTH((byte) 0x01),
-    client_certificate_url((byte) 0x02),
-    trusted_ca_keys((byte) 0x03),
-    truncated_hmac((byte) 0x04),
-    status_request((byte) 0x05),
-    user_mapping((byte) 0x06),
-    client_authz((byte) 0x07),
-    server_authz((byte) 0x08),
-    cert_type((byte) 0x09),
-    supported_groups((byte) 0x0A),
-    ec_point_formats((byte) 0x0B),
-    srp((byte) 0x0C),
-    signature_algorithms((byte) 0x0D),
-    use_srtp((byte) 0x0E),
-    heartbeat((byte) 0x0F);
-//     TODO copy all extension type from  http://www.iana.org/assignments/tls-extensiontype-values/tls-extensiontype-values.xhtml
+    SERVER_NAME("server_name", new byte[] {(byte)0x00, (byte) 0x00}),
+    MAX_FRAGMENT_LENGTH("max_fragment_length", new byte[] {(byte) 0x00, (byte) 0x01}),
+    CLIENT_CERTIFICATE_URL("client_certificate_url", new byte[] {(byte) 0x00, (byte) 0x02}),
+    TRUSTED_CA_KEYS("trusted_ca_keys", new byte[] {(byte) 0x00, (byte) 0x03}),
+    TRUNCATED_HMAC("truncated_hmac", new byte[] {(byte) 0x00, (byte) 0x04}),
+    STATUS_REQUEST("status_request", new byte[] {(byte) 0x00, (byte) 0X05}),
+    USER_MAPPING("user_mapping", new byte[] {(byte) 0x00, (byte) 0x06}),
+    CLIENT_AUTHZ("client_authz", new byte[] {(byte) 0x00, (byte) 0x07}),
+    SERVER_AUTHZ("server_authz", new byte[] {(byte) 0x00, (byte) 0x08}),
+    CERT_TYPE("cert_type", new byte[] {(byte) 0x00, (byte) 0x09}),
+    SUPPORTED_GROUPS("supported_groups", new byte[] {(byte) 0x00, (byte) 0x0A}),
+    EC_POINT_FORMATS("ec_point_formats", new byte[] {(byte) 0x00, (byte) 0x0B}),
+    SRP("srp", new byte[] {(byte) 0x00, (byte) 0x0C}),
+    SIGNATURE_ALGORITHMS("signature_algorithms", new byte[] {(byte) 0x00, (byte) 0x0D}),
+    USE_SRTP("use_srtp", new byte[] {(byte) 0x00, (byte) 0x0E}),
+    HEARTBEAT("heartbeat", new byte[] {(byte) 0x00, (byte) 0x0F}),
+    APPLICATION_LAYER_PROTOCOL_NEGOTIATION("application_layer_protocol_negotiation", new byte[] {(byte) 0x00, (byte) 0x10}),
+    STATUS_REQUEST_V2("status_request_v2", new byte[] {(byte) 0x00, (byte) 0x11}),
+    SIGNED_CERTIFICATE_TIMESTAMP("↑", new byte[] {(byte) 0x00, (byte) 0x12}),
+    CLIENT_CERTIFICATE_TYPE("client_certificate_type", new byte[] {(byte) 0x00, (byte) 0x13}),
+    SERVER_CERTIFICATE_TYPE("server_certificate_type", new byte[] {(byte) 0x00, (byte) 0x14}),
+    PADDING("padding", new byte[] {(byte) 0x00, (byte) 0x15}),
+    ENCRYPT_THEN_MAC("encrypt_then_mac", new byte[] {(byte) 0x00, (byte) 0x16}),
+    EXTENDED_MASTER_SECRET("extended_master_secret", new byte[] {(byte) 0x00, (byte) 0x17}),
+    SESSION_TICKET_TLS("session_ticket_tls", new byte[] {(byte) 0x00, (byte) 0x23}),
+    RENEGOTIATION_INFO("renegotiation_info", new byte[] {(byte) 0xFF, (byte) 0x01});
 
-    private byte value;
+    private String name;
+    private byte[] value;
 
-    ExtensionType(byte value) {
+
+    ExtensionType(String name, byte[] value) {
+        this.name = name;
         this.value = value;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public byte[] getValue() {
+        return value;
+    }
+
+    public static String getNameByByte(byte[] version) {
+        for (ExtensionType e : ExtensionType.values()) {
+            if (Arrays.equals(version, e.getValue())) {
+                return e.getName();
+            }
+        }
+        return null;
     }
 }
