18893ab48c05cf072954f3f2697c259dec7f83ae
Eduardo Acha <eduardoacha@gmail.com>
2015-11-02 11:41:18 -0300

    Added new test
    - POP3
    -IMAP4

---
+++ src/test/java/cl/tesis/mail/IMAPTest.java
@@ -0,0 +1,51 @@
+package cl.tesis.mail;
+
+import cl.tesis.ssl.HostCertificate;
+import cl.tesis.tls.ScanCiphersSuites;
+import cl.tesis.tls.ScanTLSVersion;
+import cl.tesis.tls.TLS;
+import junit.framework.TestCase;
+
+public class IMAPTest extends TestCase {
+    public static String START_PROTOCOL = "* OK [CAPABILITY IMAP4REV1 I18NLEVEL=1 LITERAL+ SASL-IR LOGIN-REFERRALS STARTTLS LOGINDISABLED] dichato.dcc.uchile.cl";
+
+    public IMAP imap;
+
+    public void setUp() throws Exception {
+        super.setUp();
+        imap = new IMAP("192.80.24.4");
+    }
+
+   public void testIMAPProtocol() throws Exception {
+        String start =  imap.startProtocol();
+        assertEquals(true, start.contains(START_PROTOCOL));
+    }
+
+    public void testHandshake() throws Exception {
+        imap.startProtocol();
+        TLS tls =  new TLS(imap.getSocket());
+        HostCertificate hostCertificate = tls.doProtocolHandshake(StartTLS.IMAP);
+        assertEquals(true, hostCertificate.isValidation());
+        assertEquals("RapidSSL SHA256 CA - G4", hostCertificate.getCertificateAuthority());
+    }
+
+    public void testAllTLSVersion() throws Exception {
+        TLS tls =  new TLS(imap.getSocket());
+        ScanTLSVersion version =  tls.checkTLSVersions(StartTLS.IMAP);
+        assertEquals(false, version.isSSL_30());
+        assertEquals(true, version.isTLS_10());
+        assertEquals(false, version.isTLS_11());
+        assertEquals(false, version.isTLS_12());
+    }
+
+    public void testCipherSuite() throws Exception {
+        TLS tls =  new TLS(imap.getSocket());
+        ScanCiphersSuites suites = tls.checkCipherSuites(StartTLS.IMAP);
+        System.out.println(suites.toJson());
+        assertEquals("TLS_RSA_EXPORT_WITH_DES40_CBC_SHA", suites.getExport_40_ciphers());
+        assertEquals("TLS_RSA_WITH_SEED_CBC_SHA", suites.getMedium_ciphers());
+        assertEquals("TLS_RSA_WITH_3DES_EDE_CBC_SHA", suites.getDes3_ciphers());
+        assertEquals("TLS_RSA_WITH_AES_256_CBC_SHA", suites.getHigh_ciphers());
+    }
+
+}
---
+++ src/test/java/cl/tesis/mail/POPTest.java
@@ -0,0 +1,46 @@
+package cl.tesis.mail;
+
+import cl.tesis.ssl.HostCertificate;
+import cl.tesis.tls.ScanCiphersSuites;
+import cl.tesis.tls.ScanTLSVersion;
+import cl.tesis.tls.TLS;
+import junit.framework.TestCase;
+
+public class POPTest extends TestCase{
+    public POP3 pop;
+
+    public void setUp() throws Exception {
+        super.setUp();
+        pop = new POP3("64.64.18.121", 110);
+    }
+
+    public void testPOPProtocol() throws Exception {
+        String start =  pop.startProtocol();
+        assertEquals("+OK Dovecot ready.\r\n", start);
+    }
+
+    public void testHandshake() throws Exception {
+        pop.startProtocol();
+        TLS tls =  new TLS(pop.getSocket());
+        HostCertificate hostCertificate = tls.doProtocolHandshake(StartTLS.POP3);
+        assertEquals(true, hostCertificate.isValidation());
+        assertEquals("Go Daddy Secure Certificate Authority - G2", hostCertificate.getCertificateAuthority());
+    }
+
+    public void testAllTLSVersion() throws Exception {
+        TLS tls =  new TLS(pop.getSocket());
+        ScanTLSVersion version =  tls.checkTLSVersions(StartTLS.POP3);
+        assertEquals(false, version.isSSL_30());
+        assertEquals(true, version.isTLS_10());
+        assertEquals(false, version.isTLS_11());
+        assertEquals(false, version.isTLS_12());
+    }
+
+    public void testCipherSuite() throws Exception {
+        TLS tls =  new TLS(pop.getSocket());
+        ScanCiphersSuites suites = tls.checkCipherSuites(StartTLS.POP3);
+        assertEquals("TLS_RSA_WITH_RC4_128_SHA", suites.getMedium_ciphers());
+        assertEquals("TLS_DHE_RSA_WITH_3DES_EDE_CBC_SHA", suites.getDes3_ciphers());
+        assertEquals("TLS_DHE_RSA_WITH_AES_256_CBC_SHA", suites.getHigh_ciphers());
+    }
+}
