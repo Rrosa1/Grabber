a7cd88ae05db1a755eeb4498803cef1b7c76bb7f
Eduardo Acha <eduardoacha@gmail.com>
2015-11-09 01:06:59 -0300

    Added connection exception

---
+++ src/main/java/cl/tesis/mail/ConnectionException.java
@@ -0,0 +1,12 @@
+package cl.tesis.mail;
+
+
+public class ConnectionException extends Exception {
+
+    public ConnectionException() {
+    }
+
+    public ConnectionException(String message) {
+        super(message);
+    }
+}
