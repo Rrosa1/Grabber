c2f45d41c81d7a90ffb606c7eff6fa6979a1ae1f
Eduardo Acha <eduardoacha@gmail.com>
2015-12-16 16:15:13 -0300

    Added BEAST cipher suites

--- src/main/java/cl/tesis/tls/constant/TLSCipher.java
+++ src/main/java/cl/tesis/tls/constant/TLSCipher.java
@@ -1,11 +1,6 @@
 package cl.tesis.tls.constant;
 
 public enum TLSCipher {
-//    TEST(CipherSuites.TLS_DHE_RSA_WITH_AES_128_CBC_SHA.getStringValue() + CipherSuites.TLS_ECDHE_RSA_WITH_RC4_128_SHA.getStringValue() +
-//            CipherSuites.TLS_RSA_WITH_NULL_MD5.getStringValue() + CipherSuites.TLS_RSA_WITH_NULL_SHA.getStringValue() +
-//            CipherSuites.TLS_RSA_WITH_NULL_SHA256.getStringValue() + CipherSuites.TLS_RSA_WITH_RC4_128_MD5.getStringValue() +
-//            CipherSuites.TLS_RSA_WITH_RC4_128_SHA.getStringValue() + CipherSuites.TLS_RSA_WITH_3DES_EDE_CBC_SHA.getStringValue() +
-//            CipherSuites.TLS_RSA_WITH_AES_128_CBC_SHA.getStringValue() + CipherSuites.TLS_RSA_WITH_AES_256_CBC_SHA.getStringValue()),
     NULL_CIPHERS(ScanMode.Normal, CipherSuites.TLS_ECDHE_RSA_WITH_NULL_SHA.getStringValue() + CipherSuites.TLS_ECDHE_ECDSA_WITH_NULL_SHA.getStringValue() +
             CipherSuites.TLS_ECDH_ANON_WITH_NULL_SHA.getStringValue() + CipherSuites.TLS_ECDH_RSA_WITH_NULL_SHA.getStringValue() +
             CipherSuites.TLS_ECDH_ECDSA_WITH_NULL_SHA.getStringValue() + CipherSuites.TLS_RSA_WITH_NULL_SHA256.getStringValue() +
@@ -75,7 +70,32 @@ public enum TLSCipher {
             CipherSuites.TLS_RSA_EXPORT_WITH_DES40_CBC_SHA.getStringValue() + CipherSuites.TLS_RSA_EXPORT_WITH_RC2_CBC_40_MD5.getStringValue() +
             CipherSuites.TLS_RSA_EXPORT_WITH_RC4_40_MD5 .getStringValue()),
     LOGJAM(ScanMode.Normal, CipherSuites.TLS_DHE_DSS_EXPORT1024_WITH_DES_CBC_SHA.getStringValue() + CipherSuites.TLS_DHE_DSS_EXPORT1024_WITH_RC4_56_SHA.getStringValue() +
-            CipherSuites.TLS_DHE_RSA_EXPORT_WITH_DES40_CBC_SHA.getStringValue() + CipherSuites.TLS_DHE_DSS_EXPORT_WITH_DES40_CBC_SHA.getStringValue());
+            CipherSuites.TLS_DHE_RSA_EXPORT_WITH_DES40_CBC_SHA.getStringValue() + CipherSuites.TLS_DHE_DSS_EXPORT_WITH_DES40_CBC_SHA.getStringValue()),
+    BEAST(ScanMode.Other, CipherSuites.TLS_SRP_SHA_DSS_WITH_AES_256_CBC_SHA.getStringValue() + CipherSuites.TLS_SRP_SHA_RSA_WITH_AES_256_CBC_SHA.getStringValue() +
+            CipherSuites.TLS_SRP_SHA_WITH_AES_256_CBC_SHA.getStringValue() + CipherSuites.TLS_PSK_WITH_3DES_EDE_CBC_SHA.getStringValue() +
+            CipherSuites.TLS_SRP_SHA_DSS_WITH_AES_128_CBC_SHA.getStringValue() + CipherSuites.TLS_SRP_SHA_RSA_WITH_AES_128_CBC_SHA.getStringValue() +
+            CipherSuites.TLS_SRP_SHA_WITH_AES_128_CBC_SHA.getStringValue() + CipherSuites.TLS_RSA_WITH_IDEA_CBC_SHA.getStringValue() +
+            CipherSuites.TLS_PSK_WITH_AES_128_CBC_SHA.getStringValue() + CipherSuites.TLS_KRB5_WITH_IDEA_CBC_SHA.getStringValue() +
+            CipherSuites.TLS_KRB5_WITH_IDEA_CBC_MD5.getStringValue() + CipherSuites.TLS_ECDHE_RSA_WITH_3DES_EDE_CBC_SHA.getStringValue() +
+            CipherSuites.TLS_ECDHE_ECDSA_WITH_3DES_EDE_CBC_SHA.getStringValue() + CipherSuites.TLS_SRP_SHA_DSS_WITH_3DES_EDE_CBC_SHA.getStringValue() +
+            CipherSuites.TLS_SRP_SHA_RSA_WITH_3DES_EDE_CBC_SHA.getStringValue() + CipherSuites.TLS_SRP_SHA_WITH_3DES_EDE_CBC_SHA.getStringValue() +
+            CipherSuites.TLS_DHE_RSA_WITH_3DES_EDE_CBC_SHA.getStringValue() + CipherSuites.TLS_DHE_DSS_WITH_3DES_EDE_CBC_SHA.getStringValue() +
+            CipherSuites.TLS_DH_RSA_WITH_3DES_EDE_CBC_SHA.getStringValue() + CipherSuites.TLS_DH_DSS_WITH_3DES_EDE_CBC_SHA.getStringValue() +
+            CipherSuites.TLS_ECDH_ANON_WITH_3DES_EDE_CBC_SHA.getStringValue() + CipherSuites.TLS_DH_ANON_WITH_3DES_EDE_CBC_SHA.getStringValue() +
+            CipherSuites.TLS_ECDH_RSA_WITH_3DES_EDE_CBC_SHA.getStringValue() + CipherSuites.TLS_ECDH_ECDSA_WITH_3DES_EDE_CBC_SHA.getStringValue() +
+            CipherSuites.TLS_RSA_WITH_3DES_EDE_CBC_SHA.getStringValue() + CipherSuites.TLS_PSK_WITH_3DES_EDE_CBC_SHA.getStringValue() +
+            CipherSuites.TLS_KRB5_WITH_3DES_EDE_CBC_SHA.getStringValue() + CipherSuites.TLS_KRB5_WITH_3DES_EDE_CBC_SHA.getStringValue() +
+            CipherSuites.TLS_DHE_DSS_EXPORT1024_WITH_DES_CBC_SHA.getStringValue() + CipherSuites.TLS_DHE_RSA_WITH_DES_CBC_SHA.getStringValue() +
+            CipherSuites.TLS_DHE_DSS_WITH_DES_CBC_SHA.getStringValue() + CipherSuites.TLS_DH_RSA_WITH_DES_CBC_SHA.getStringValue() +
+            CipherSuites.TLS_DH_DSS_WITH_DES_CBC_SHA.getStringValue() + CipherSuites.TLS_DH_ANON_WITH_DES_CBC_SHA.getStringValue() +
+            CipherSuites.TLS_RSA_EXPORT1024_WITH_DES_CBC_SHA.getStringValue() + CipherSuites.TLS_RSA_WITH_DES_CBC_SHA.getStringValue() +
+            CipherSuites.TLS_KRB5_WITH_DES_CBC_SHA.getStringValue() + CipherSuites.TLS_KRB5_WITH_DES_CBC_MD5.getStringValue() +
+            CipherSuites.TLS_DHE_RSA_EXPORT_WITH_DES40_CBC_SHA.getStringValue() + CipherSuites.TLS_DHE_DSS_EXPORT_WITH_DES40_CBC_SHA.getStringValue() +
+            CipherSuites.TLS_DH_RSA_EXPORT_WITH_DES40_CBC_SHA.getStringValue() + CipherSuites.TLS_DH_DSS_EXPORT_WITH_DES40_CBC_SHA.getStringValue() +
+            CipherSuites.TLS_DH_ANON_EXPORT_WITH_DES40_CBC_SHA.getStringValue() + CipherSuites.TLS_RSA_EXPORT_WITH_DES40_CBC_SHA.getStringValue() +
+            CipherSuites.TLS_RSA_EXPORT_WITH_RC2_CBC_40_MD5.getStringValue() + CipherSuites.TLS_KRB5_EXPORT_WITH_RC2_CBC_40_SHA.getStringValue() +
+            CipherSuites.TLS_KRB5_EXPORT_WITH_DES_CBC_40_SHA.getStringValue() + CipherSuites.TLS_KRB5_EXPORT_WITH_RC2_CBC_40_MD5.getStringValue() +
+            CipherSuites.TLS_KRB5_EXPORT_WITH_DES_CBC_40_MD5.getStringValue());
 
 
     private ScanMode scanMode;
